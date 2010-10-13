ff7f5680ac190bbf35cba216c45dcd2481d15e87
bullfight <p.schmitz@gmail.com>
2010-10-13 15:18:31 -0500

    pushed out gist for working with rolling averages

---
+++ functions/roll.avg.R
@@ -0,0 +1,9 @@
+# Produce Rolling Averages ############################################
+#library(zoo)
+
+#vals <- as.matrix(dat[3:37], rownames.force = T)
+#vals <- as.matrix(dat[3:dim(dat)[2]], rownames.force = T)
+#dat.ts <- zoo(vals, dat$TIMESTAMP)
+
+#m.dat <- rollmean(dat.ts,30, na.pad = T, align = "right")
+#######################################################################
